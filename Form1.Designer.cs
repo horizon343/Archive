5f505abbb0753dfef022464f9f9b03c07e185709
norply <75418405+norply@users.noreply.github.com>
2023-07-17 16:01:14 +0500

    Интерфейс

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,25 +30,140 @@ namespace Archive
         /// </summary>
         private void InitializeComponent()
         {
+            panelMenu = new Panel();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            panelLogo = new Panel();
+            panelTitleBar = new Panel();
+            lblTitle = new Label();
+            panelDesktopPanel = new Panel();
+            panelMenu.SuspendLayout();
+            panelTitleBar.SuspendLayout();
             SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            panelMenu.BackColor = Color.FromArgb(51, 51, 76);
+            panelMenu.Controls.Add(button3);
+            panelMenu.Controls.Add(button2);
+            panelMenu.Controls.Add(button1);
+            panelMenu.Controls.Add(panelLogo);
+            panelMenu.Dock = DockStyle.Left;
+            panelMenu.Location = new Point(0, 0);
+            panelMenu.Name = "panelMenu";
+            panelMenu.Size = new Size(220, 463);
+            panelMenu.TabIndex = 0;
+            // 
+            // button3
+            // 
+            button3.Dock = DockStyle.Top;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.ForeColor = Color.Gainsboro;
+            button3.Location = new Point(0, 200);
+            button3.Name = "button3";
+            button3.Size = new Size(220, 60);
+            button3.TabIndex = 3;
+            button3.Text = "МКБ";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button2
+            // 
+            button2.Dock = DockStyle.Top;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.ForeColor = Color.Gainsboro;
+            button2.Location = new Point(0, 140);
+            button2.Name = "button2";
+            button2.Size = new Size(220, 60);
+            button2.TabIndex = 2;
+            button2.Text = "Пациенты";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.Dock = DockStyle.Top;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.ForeColor = Color.Gainsboro;
+            button1.Location = new Point(0, 80);
+            button1.Name = "button1";
+            button1.Size = new Size(220, 60);
+            button1.TabIndex = 1;
+            button1.Text = "Карты";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // panelLogo
+            // 
+            panelLogo.BackColor = Color.FromArgb(39, 39, 58);
+            panelLogo.Dock = DockStyle.Top;
+            panelLogo.Location = new Point(0, 0);
+            panelLogo.Name = "panelLogo";
+            panelLogo.Size = new Size(220, 80);
+            panelLogo.TabIndex = 0;
+            // 
+            // panelTitleBar
+            // 
+            panelTitleBar.BackColor = Color.FromArgb(0, 150, 136);
+            panelTitleBar.Controls.Add(lblTitle);
+            panelTitleBar.Dock = DockStyle.Top;
+            panelTitleBar.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            panelTitleBar.Location = new Point(220, 0);
+            panelTitleBar.Name = "panelTitleBar";
+            panelTitleBar.Size = new Size(745, 80);
+            panelTitleBar.TabIndex = 1;
+            panelTitleBar.Paint += panelTitleBar_Paint;
+            // 
+            // lblTitle
+            // 
+            lblTitle.Anchor = AnchorStyles.None;
+            lblTitle.AutoSize = true;
+            lblTitle.Location = new Point(317, 27);
+            lblTitle.Name = "lblTitle";
+            lblTitle.RightToLeft = RightToLeft.No;
+            lblTitle.Size = new Size(76, 30);
+            lblTitle.TabIndex = 0;
+            lblTitle.Text = "Меню";
+            lblTitle.Click += lblTitle_Click;
+            // 
+            // panelDesktopPanel
+            // 
+            panelDesktopPanel.Dock = DockStyle.Fill;
+            panelDesktopPanel.Location = new Point(220, 80);
+            panelDesktopPanel.Name = "panelDesktopPanel";
+            panelDesktopPanel.Size = new Size(745, 383);
+            panelDesktopPanel.TabIndex = 2;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(965, 507);
+            ClientSize = new Size(965, 463);
+            Controls.Add(panelDesktopPanel);
+            Controls.Add(panelTitleBar);
+            Controls.Add(panelMenu);
+            ForeColor = Color.White;
             Name = "Form1";
             Text = "Form1";
+            panelMenu.ResumeLayout(false);
+            panelTitleBar.ResumeLayout(false);
+            panelTitleBar.PerformLayout();
             ResumeLayout(false);
-
-
-            //DB test
-            DBase DBase = new DBase();
-            DBase.CreateTables();
-            var a = DBase.GetTable<MKBItem>();
-            DBase.SetDataTable<MKBItem>(new MKBItem() { MKBCode = "7", Title = "1234"});
         }
 
         #endregion
+
+        private Panel panelMenu;
+        private Button button1;
+        private Button button3;
+        private Button button2;
+        private Panel panelLogo;
+        private Panel panelTitleBar;
+        private Label lblTitle;
+        private Panel panelDesktopPanel;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,97 @@ namespace Archive
 {
     public partial class Form1 : Form
     {
+        private Button currentButton;
+        private int tempIndex;
+        private Form activeForm;
+
         public Form1()
         {
             InitializeComponent();
+        }
+        private void ActivateButton(object btnSender)
+        {
+            if (btnSender != null)
+            {
+                if (currentButton != (Button)btnSender)
+                {
+                    DisableButton();
+                    currentButton = (Button)btnSender;
+                    currentButton.BackColor = Color.Orange;
+                    currentButton.ForeColor = Color.White;
+                    currentButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+
+                }
+            }
+
+        }
+        private void DisableButton()
+        {
+            foreach (Control previousBtn in panelMenu.Controls)
+            {
+                if (previousBtn.GetType() == typeof(Button))
+                {
+                    previousBtn.BackColor = Color.FromArgb(51, 51, 76);
+                    previousBtn.ForeColor = Color.Gainsboro;
+                    previousBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                }
+            }
+        }
+
+        private void OpenChildForm(Form childForm, object btnSender)
+        {
+            if (activeForm != null)
+                activeForm.Close();
+            ActivateButton(btnSender);
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            this.panelDesktopPanel.Controls.Add(childForm);
+            this.panelDesktopPanel.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+            lblTitle.Text = childForm.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.FormRecords(), sender);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.FormPatients(), sender);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.FormMKB(), sender);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void panelTitleBar_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void lblTitle_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
