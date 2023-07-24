0bfba165ca8b3fb59d5f62d0a7daeb70782d0cbd
norply <75418405+norply@users.noreply.github.com>
2023-07-24 05:10:22 +0500

    FormMKB

--- Forms/FormMKB.Designer.cs
+++ Forms/FormMKB.Designer.cs
@@ -28,70 +28,171 @@
         /// </summary>
         private void InitializeComponent()
         {
+            MKBTable = new DataGridView();
+            panel1 = new Panel();
+            NextPageButton = new Button();
+            PrevPageButton = new Button();
+            CountPageTextBox = new Label();
+            TextError = new Label();
+            panel3 = new Panel();
             label1 = new Label();
-            textBox1 = new TextBox();
-            comboBox1 = new ComboBox();
-            dataGridView1 = new DataGridView();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            MKBCodeTextField = new TextBox();
+            SearchButton = new Button();
+            DiseaseTextField = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)MKBTable).BeginInit();
+            panel1.SuspendLayout();
+            panel3.SuspendLayout();
             SuspendLayout();
+            // 
+            // MKBTable
+            // 
+            MKBTable.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            MKBTable.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            MKBTable.Location = new Point(12, 49);
+            MKBTable.Name = "MKBTable";
+            MKBTable.RowTemplate.Height = 25;
+            MKBTable.Size = new Size(935, 352);
+            MKBTable.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.Bottom;
+            panel1.Controls.Add(NextPageButton);
+            panel1.Controls.Add(PrevPageButton);
+            panel1.Controls.Add(CountPageTextBox);
+            panel1.Location = new Point(309, 407);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(341, 29);
+            panel1.TabIndex = 10;
+            // 
+            // NextPageButton
+            // 
+            NextPageButton.FlatStyle = FlatStyle.Flat;
+            NextPageButton.ForeColor = Color.Black;
+            NextPageButton.Location = new Point(213, 3);
+            NextPageButton.Name = "NextPageButton";
+            NextPageButton.Size = new Size(123, 23);
+            NextPageButton.TabIndex = 9;
+            NextPageButton.Text = "Следующая";
+            NextPageButton.UseVisualStyleBackColor = true;
+            // 
+            // PrevPageButton
+            // 
+            PrevPageButton.FlatStyle = FlatStyle.Flat;
+            PrevPageButton.ForeColor = Color.Black;
+            PrevPageButton.Location = new Point(0, 3);
+            PrevPageButton.Name = "PrevPageButton";
+            PrevPageButton.Size = new Size(123, 23);
+            PrevPageButton.TabIndex = 8;
+            PrevPageButton.Text = "Предыдущая";
+            PrevPageButton.UseVisualStyleBackColor = true;
+            // 
+            // CountPageTextBox
+            // 
+            CountPageTextBox.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            CountPageTextBox.AutoSize = true;
+            CountPageTextBox.ForeColor = Color.Black;
+            CountPageTextBox.Location = new Point(131, 7);
+            CountPageTextBox.MinimumSize = new Size(75, 0);
+            CountPageTextBox.Name = "CountPageTextBox";
+            CountPageTextBox.Size = new Size(75, 15);
+            CountPageTextBox.TabIndex = 4;
+            CountPageTextBox.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // TextError
+            // 
+            TextError.AutoSize = true;
+            TextError.BackColor = Color.Transparent;
+            TextError.ForeColor = Color.Black;
+            TextError.Location = new Point(85, -1);
+            TextError.Name = "TextError";
+            TextError.Size = new Size(0, 15);
+            TextError.TabIndex = 11;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.Transparent;
+            panel3.Controls.Add(label1);
+            panel3.Controls.Add(MKBCodeTextField);
+            panel3.Controls.Add(SearchButton);
+            panel3.Controls.Add(DiseaseTextField);
+            panel3.Location = new Point(12, 12);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(348, 31);
+            panel3.TabIndex = 12;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.ForeColor = Color.Black;
-            label1.Location = new Point(81, 15);
+            label1.Location = new Point(3, 8);
             label1.Name = "label1";
             label1.Size = new Size(42, 15);
-            label1.TabIndex = 9;
+            label1.TabIndex = 4;
             label1.Text = "Поиск";
             // 
-            // textBox1
+            // MKBCodeTextField
             // 
-            textBox1.Location = new Point(129, 13);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
-            textBox1.TabIndex = 7;
+            MKBCodeTextField.Location = new Point(47, 5);
+            MKBCodeTextField.Name = "MKBCodeTextField";
+            MKBCodeTextField.PlaceholderText = "Код";
+            MKBCodeTextField.Size = new Size(90, 23);
+            MKBCodeTextField.TabIndex = 1;
             // 
-            // comboBox1
+            // SearchButton
             // 
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Items.AddRange(new object[] { "Код", "Диагноз" });
-            comboBox1.Location = new Point(235, 13);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(98, 23);
-            comboBox1.TabIndex = 6;
+            SearchButton.FlatStyle = FlatStyle.Flat;
+            SearchButton.ForeColor = Color.Black;
+            SearchButton.Location = new Point(269, 5);
+            SearchButton.Name = "SearchButton";
+            SearchButton.Size = new Size(75, 23);
+            SearchButton.TabIndex = 6;
+            SearchButton.Text = "Поиск";
+            SearchButton.UseVisualStyleBackColor = true;
+            SearchButton.Click += SearchButton_Click;
             // 
-            // dataGridView1
+            // DiseaseTextField
             // 
-            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(12, 42);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowTemplate.Height = 25;
-            dataGridView1.Size = new Size(776, 397);
-            dataGridView1.TabIndex = 5;
+            DiseaseTextField.Location = new Point(143, 5);
+            DiseaseTextField.Name = "DiseaseTextField";
+            DiseaseTextField.PlaceholderText = "Болезнь";
+            DiseaseTextField.Size = new Size(120, 23);
+            DiseaseTextField.TabIndex = 2;
             // 
             // FormMKB
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(label1);
-            Controls.Add(textBox1);
-            Controls.Add(comboBox1);
-            Controls.Add(dataGridView1);
+            ClientSize = new Size(959, 448);
+            Controls.Add(TextError);
+            Controls.Add(panel3);
+            Controls.Add(panel1);
+            Controls.Add(MKBTable);
             Name = "FormMKB";
             Text = "МКБ";
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)MKBTable).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
+        private DataGridView MKBTable;
+        private Panel panel1;
+        private Button NextPageButton;
+        private Button PrevPageButton;
+        private Label CountPageTextBox;
+        private Label TextError;
+        private Panel panel3;
         private Label label1;
-        private TextBox textBox1;
-        private ComboBox comboBox1;
-        private DataGridView dataGridView1;
+        private TextBox MKBCodeTextField;
+        private TextBox DateOfBirthTextField;
+        private TextBox MiddleNameTextField;
+        private Button SearchButton;
+        private TextBox FirstNameTextField;
+        private TextBox DiseaseTextField;
     }
 }
--- Forms/FormMKB.cs
+++ Forms/FormMKB.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Archive.DB;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,137 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Archive.Validation;
 
 namespace Archive.Forms
 {
     public partial class FormMKB : Form
     {
+        public int TotalCount { get; set; } = 0;
+        public int CurrentPage { get; set; } = 1;
+        private int limit = 100;
         public FormMKB()
         {
             InitializeComponent();
+
+            MKBTableInit(CurrentPage, limit);
+
+            DataGridViewCellStyle cellStyle = new DataGridViewCellStyle
+            {
+                BackColor = Color.White,
+                ForeColor = Color.Black,
+            };
+
+
+
+            foreach (DataGridViewColumn column in MKBTable.Columns)
+                column.ReadOnly = true;
+
+            PrevPageButton.Enabled = false;
+            if (CurrentPage >= TotalCount)
+                NextPageButton.Enabled = false;
+
+
+            MKBTable.Columns[0].HeaderText = "Код";
+            MKBTable.Columns[1].HeaderText = "Болезнь";
+            MKBTable.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            MKBTable.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            MKBTable.DefaultCellStyle = cellStyle;
+
         }
+        private void data_show()
+        {
+
+
+        }
+        private void MKBTableInit(int currentPage, int limit)
+        {
+            // Получение таблицы пациентов из базы данных
+            DBase dBase = new DBase();
+            (int, List<MKBItem>) MKB = dBase.GetTable<MKBItem>(currentPage, limit);
+            dBase.CloseDatabaseConnection();
+            TotalCount = MKB.Item1;
+
+            // Выбираем только необходимые поля и добавляем пациентов втаблицу
+            List<MKBViewItem> MKBDataSource = MKB.Item2.Select(MKB => new MKBViewItem()
+            {
+                MKBCode = MKB.MKBCode,
+                Title = MKB.Title
+            }).ToList();
+            MKBTable.DataSource = MKBDataSource;
+
+            CountPageTextBox.Text = $"{CurrentPage} / {TotalCount}";
+        }
+
+        private void NextPageButton_Click(object sender, EventArgs e)
+        {
+            if (CurrentPage + 1 <= TotalCount)
+            {
+                CurrentPage += 1;
+                PrevPageButton.Enabled = true;
+                MKBTableInit(CurrentPage, limit);
+
+                if (CurrentPage + 1 > TotalCount)
+                    NextPageButton.Enabled = false;
+            }
+        }
+
+        private void PrevPageButton_Click(object sender, EventArgs e)
+        {
+            if (CurrentPage - 1 >= 1)
+            {
+                CurrentPage -= 1;
+                NextPageButton.Enabled = true;
+                MKBTableInit(CurrentPage, limit);
+
+                if (CurrentPage - 1 < 1)
+                    PrevPageButton.Enabled = false;
+            }
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            TotalCount = 1;
+            CurrentPage = 1;
+
+            Dictionary<string, object> fields = new Dictionary<string, object>();
+            if (MKBCodeTextField.Text != "")
+                fields.Add("MKBCode", MKBCodeTextField.Text);
+            if (DiseaseTextField.Text != "")
+                fields.Add("Title", DiseaseTextField.Text);
+
+
+            if (fields.Count != 0)
+            {
+                DBase dBase = new DBase();
+                List<MKBItem> MKB = dBase.SearchData<MKBItem>(fields);
+                dBase.CloseDatabaseConnection();
+
+                List<MKBViewItem> MKBDataSource = MKB.Select(MKB => new MKBViewItem()
+                {
+                    MKBCode = MKB.MKBCode,
+                    Title = MKB.Title
+                }).ToList();
+                MKBTable.DataSource = MKBDataSource;
+
+                CountPageTextBox.Text = $"1 / 1";
+                PrevPageButton.Enabled = false;
+                NextPageButton.Enabled = false;
+            }
+            else
+            {
+                MKBTableInit(CurrentPage, limit);
+                PrevPageButton.Enabled = false;
+                NextPageButton.Enabled = true;
+            }
+        }
+    }
+
+
+    class MKBViewItem
+    {
+        public string MKBCode { get; set; }
+        public string Title { get; set; }
+
     }
 }
