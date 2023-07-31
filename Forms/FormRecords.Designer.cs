c4ebf5883b5df14931d539d58c0f964bee318e57
norply <75418405+norply@users.noreply.github.com>
2023-07-31 11:18:18 +0500

    FormRecords

--- Forms/FormRecords.Designer.cs
+++ Forms/FormRecords.Designer.cs
@@ -28,70 +28,203 @@
         /// </summary>
         private void InitializeComponent()
         {
+            RecordsTable = new DataGridView();
+            panel3 = new Panel();
             label1 = new Label();
-            textBox1 = new TextBox();
-            comboBox1 = new ComboBox();
-            dataGridView1 = new DataGridView();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            HistoryNumberTextField = new TextBox();
+            MKBCodeTextField = new TextBox();
+            DateOfDischargeTextField = new TextBox();
+            SearchButton = new Button();
+            DateOfReceiptTextField = new TextBox();
+            DepartmentTextField = new TextBox();
+            panel1 = new Panel();
+            NextPageButton = new Button();
+            PrevPageButton = new Button();
+            CountPageTextBox = new Label();
+            TextError = new Label();
+            ((System.ComponentModel.ISupportInitialize)RecordsTable).BeginInit();
+            panel3.SuspendLayout();
+            panel1.SuspendLayout();
             SuspendLayout();
+            // 
+            // RecordsTable
+            // 
+            RecordsTable.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            RecordsTable.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            RecordsTable.Location = new Point(12, 50);
+            RecordsTable.Name = "RecordsTable";
+            RecordsTable.RowTemplate.Height = 25;
+            RecordsTable.Size = new Size(935, 352);
+            RecordsTable.TabIndex = 5;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.Transparent;
+            panel3.Controls.Add(label1);
+            panel3.Controls.Add(HistoryNumberTextField);
+            panel3.Controls.Add(MKBCodeTextField);
+            panel3.Controls.Add(DateOfDischargeTextField);
+            panel3.Controls.Add(SearchButton);
+            panel3.Controls.Add(DateOfReceiptTextField);
+            panel3.Controls.Add(DepartmentTextField);
+            panel3.Location = new Point(12, 12);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(701, 31);
+            panel3.TabIndex = 10;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.ForeColor = Color.Black;
-            label1.Location = new Point(81, 15);
+            label1.Location = new Point(3, 8);
             label1.Name = "label1";
             label1.Size = new Size(42, 15);
-            label1.TabIndex = 9;
+            label1.TabIndex = 4;
             label1.Text = "Поиск";
             // 
-            // textBox1
+            // HistoryNumberTextField
             // 
-            textBox1.Location = new Point(129, 13);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
-            textBox1.TabIndex = 7;
+            HistoryNumberTextField.Location = new Point(47, 5);
+            HistoryNumberTextField.Name = "HistoryNumberTextField";
+            HistoryNumberTextField.PlaceholderText = "Номер";
+            HistoryNumberTextField.Size = new Size(90, 23);
+            HistoryNumberTextField.TabIndex = 1;
             // 
-            // comboBox1
+            // MKBCodeTextField
             // 
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Items.AddRange(new object[] { "Номер", "Фамилия", "Дата Рождения", "Дата начала", "Дата окончания" });
-            comboBox1.Location = new Point(235, 13);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(98, 23);
-            comboBox1.TabIndex = 6;
+            MKBCodeTextField.Location = new Point(523, 5);
+            MKBCodeTextField.Name = "MKBCodeTextField";
+            MKBCodeTextField.PlaceholderText = "МКБ";
+            MKBCodeTextField.Size = new Size(90, 23);
+            MKBCodeTextField.TabIndex = 5;
             // 
-            // dataGridView1
+            // DateOfDischargeTextField
             // 
-            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(12, 50);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowTemplate.Height = 25;
-            dataGridView1.Size = new Size(960, 463);
-            dataGridView1.TabIndex = 5;
+            DateOfDischargeTextField.Location = new Point(397, 5);
+            DateOfDischargeTextField.Name = "DateOfDischargeTextField";
+            DateOfDischargeTextField.PlaceholderText = "Дата выписки";
+            DateOfDischargeTextField.Size = new Size(120, 23);
+            DateOfDischargeTextField.TabIndex = 4;
+            DateOfDischargeTextField.TextChanged += DateOfDischargeTextField_TextChanged;
+            // 
+            // SearchButton
+            // 
+            SearchButton.FlatStyle = FlatStyle.Flat;
+            SearchButton.ForeColor = Color.Black;
+            SearchButton.Location = new Point(619, 5);
+            SearchButton.Name = "SearchButton";
+            SearchButton.Size = new Size(75, 23);
+            SearchButton.TabIndex = 6;
+            SearchButton.Text = "Поиск";
+            SearchButton.UseVisualStyleBackColor = true;
+            SearchButton.Click += SearchButton_Click;
+            // 
+            // DateOfReceiptTextField
+            // 
+            DateOfReceiptTextField.Location = new Point(271, 5);
+            DateOfReceiptTextField.Name = "DateOfReceiptTextField";
+            DateOfReceiptTextField.PlaceholderText = "Дата поступления";
+            DateOfReceiptTextField.Size = new Size(120, 23);
+            DateOfReceiptTextField.TabIndex = 3;
+            DateOfReceiptTextField.TextChanged += DateOfReceiptTextField_TextChanged;
+            // 
+            // DepartmentTextField
+            // 
+            DepartmentTextField.Location = new Point(143, 5);
+            DepartmentTextField.Name = "DepartmentTextField";
+            DepartmentTextField.PlaceholderText = "Отделение";
+            DepartmentTextField.Size = new Size(120, 23);
+            DepartmentTextField.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.Bottom;
+            panel1.Controls.Add(NextPageButton);
+            panel1.Controls.Add(PrevPageButton);
+            panel1.Controls.Add(CountPageTextBox);
+            panel1.Location = new Point(309, 408);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(341, 29);
+            panel1.TabIndex = 11;
+            // 
+            // NextPageButton
+            // 
+            NextPageButton.FlatStyle = FlatStyle.Flat;
+            NextPageButton.ForeColor = Color.Black;
+            NextPageButton.Location = new Point(213, 3);
+            NextPageButton.Name = "NextPageButton";
+            NextPageButton.Size = new Size(123, 23);
+            NextPageButton.TabIndex = 9;
+            NextPageButton.Text = "Следующая";
+            NextPageButton.UseVisualStyleBackColor = true;
+            // 
+            // PrevPageButton
+            // 
+            PrevPageButton.FlatStyle = FlatStyle.Flat;
+            PrevPageButton.ForeColor = Color.Black;
+            PrevPageButton.Location = new Point(0, 3);
+            PrevPageButton.Name = "PrevPageButton";
+            PrevPageButton.Size = new Size(123, 23);
+            PrevPageButton.TabIndex = 8;
+            PrevPageButton.Text = "Предыдущая";
+            PrevPageButton.UseVisualStyleBackColor = true;
+            // 
+            // CountPageTextBox
+            // 
+            CountPageTextBox.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            CountPageTextBox.AutoSize = true;
+            CountPageTextBox.ForeColor = Color.Black;
+            CountPageTextBox.Location = new Point(131, 7);
+            CountPageTextBox.MinimumSize = new Size(75, 0);
+            CountPageTextBox.Name = "CountPageTextBox";
+            CountPageTextBox.Size = new Size(75, 15);
+            CountPageTextBox.TabIndex = 4;
+            CountPageTextBox.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // TextError
+            // 
+            TextError.AutoSize = true;
+            TextError.BackColor = Color.Transparent;
+            TextError.ForeColor = Color.Black;
+            TextError.Location = new Point(66, -1);
+            TextError.Name = "TextError";
+            TextError.Size = new Size(0, 15);
+            TextError.TabIndex = 4;
             // 
             // FormRecords
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(984, 524);
-            Controls.Add(label1);
-            Controls.Add(textBox1);
-            Controls.Add(comboBox1);
-            Controls.Add(dataGridView1);
+            ClientSize = new Size(959, 448);
+            Controls.Add(panel1);
+            Controls.Add(panel3);
+            Controls.Add(RecordsTable);
+            Controls.Add(TextError);
             Name = "FormRecords";
             Text = "Карты";
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)RecordsTable).EndInit();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
+        private DataGridView RecordsTable;
+        private Panel panel3;
         private Label label1;
-        private TextBox textBox1;
-        private ComboBox comboBox1;
-        private DataGridView dataGridView1;
+        private TextBox HistoryNumberTextField;
+        private TextBox MKBCodeTextField;
+        private TextBox DateOfDischargeTextField;
+        private Button SearchButton;
+        private TextBox DateOfReceiptTextField;
+        private TextBox DepartmentTextField;
+        private Panel panel1;
+        private Button NextPageButton;
+        private Button PrevPageButton;
+        private Label TextError;
+        private Label CountPageTextBox;
     }
 }
--- Forms/FormRecords.cs
+++ Forms/FormRecords.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Archive.DB;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,244 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Archive.Validation;
 
 namespace Archive.Forms
 {
     public partial class FormRecords : Form
     {
+        public int TotalCount { get; set; } = 0;
+        public int CurrentPage { get; set; } = 1;
+        private int limit = 100;
+        private List<RecordsViewItem> recordDataSource;
+        private int columnIndex = -1;
         public FormRecords()
         {
             InitializeComponent();
+
+            RecordsTableInit(CurrentPage, limit);
+
+            RecordsTable.CellClick += RecordsTable_CellClick;
+
+            DataGridViewCellStyle cellStyle = new DataGridViewCellStyle
+            {
+                BackColor = Color.White,
+                ForeColor = Color.Black,
+            };
+
+            foreach (DataGridViewColumn column in RecordsTable.Columns)
+                column.ReadOnly = true;
+
+            PrevPageButton.Enabled = false;
+            if (CurrentPage >= TotalCount)
+                NextPageButton.Enabled = false;
+
+            RecordsTable.Columns[0].HeaderText = "Номер Истории";
+            RecordsTable.Columns[1].HeaderText = "Отделение";
+            RecordsTable.Columns[2].HeaderText = "Дата Посупления";
+            RecordsTable.Columns[3].HeaderText = "Дата выписки";
+            RecordsTable.Columns[4].HeaderText = "Код МКБ";
+            RecordsTable.Columns[0].Width = 100;
+            RecordsTable.Columns[1].Width = 300;
+            RecordsTable.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            RecordsTable.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            RecordsTable.DefaultCellStyle = cellStyle;
+
+
+
         }
+        private void RecordsTableInit(int currentPage, int limit)
+        {
+            DBase dBase = new DBase();
+            (int, List<RecordItem>) Record = dBase.GetTable<RecordItem>(currentPage, limit);
+            dBase.CloseDatabaseConnection();
+            TotalCount = Record.Item1;
+
+            recordDataSource = Record.Item2.Select(Record => new RecordsViewItem()
+            {
+                HistoryNumber = Record.HistoryNumber,
+                DepartmentID = Record.DepartmentID,
+                DateOfReceipt = Record.DateOfReceipt,
+                DateOfDischarge = Record.DateOfDischarge,
+                MKBCode = Record.MKBCode
+
+            }).ToList();
+            RecordsTable.DataSource = recordDataSource;
+
+            CountPageTextBox.Text = $"{CurrentPage} / {TotalCount}";
+        }
+        private void NextPageButton_Click(object sender, EventArgs e)
+        {
+            if (CurrentPage + 1 <= TotalCount)
+            {
+                CurrentPage += 1;
+                PrevPageButton.Enabled = true;
+                RecordsTableInit(CurrentPage, limit);
+
+                if (CurrentPage + 1 > TotalCount)
+                    NextPageButton.Enabled = false;
+            }
+        }
+
+        private void PrevPageButton_Click(object sender, EventArgs e)
+        {
+            if (CurrentPage - 1 >= 1)
+            {
+                CurrentPage -= 1;
+                NextPageButton.Enabled = true;
+                RecordsTableInit(CurrentPage, limit);
+
+                if (CurrentPage - 1 < 1)
+                    PrevPageButton.Enabled = false;
+            }
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            TotalCount = 1;
+            CurrentPage = 1;
+
+            Dictionary<string, object> fields = new Dictionary<string, object>();
+            if (HistoryNumberTextField.Text != "")
+                fields.Add("HistoryNumber", HistoryNumberTextField.Text);
+            if (DepartmentTextField.Text != "")
+                fields.Add("DepartmentID", DepartmentTextField.Text);
+            if (DateOfReceiptTextField.Text != "")
+                fields.Add("DateOfReceipt", DateTime.Parse(DateOfReceiptTextField.Text));
+            if (DateOfDischargeTextField.Text != "")
+                fields.Add("DateOfDischarge", DateTime.Parse(DateOfDischargeTextField.Text));
+            if (MKBCodeTextField.Text != "")
+                fields.Add("MKBCode", MKBCodeTextField.Text);
+
+
+            if (fields.Count != 0)
+            {
+                DBase dBase = new DBase();
+                List<RecordItem> Record = dBase.SearchData<RecordItem>(fields);
+                dBase.CloseDatabaseConnection();
+
+                List<RecordsViewItem> RecordDataSource = Record.Select(Record => new RecordsViewItem()
+                {
+                    HistoryNumber = Record.HistoryNumber,
+                    DepartmentID = Record.DepartmentID,
+                    DateOfReceipt = Record.DateOfReceipt,
+                    DateOfDischarge = Record.DateOfDischarge,
+                    MKBCode = Record.MKBCode
+                }).ToList();
+                RecordsTable.DataSource = RecordDataSource;
+
+                CountPageTextBox.Text = $"1 / 1";
+                PrevPageButton.Enabled = false;
+                NextPageButton.Enabled = false;
+            }
+            else
+            {
+                RecordsTableInit(CurrentPage, limit);
+                PrevPageButton.Enabled = false;
+                NextPageButton.Enabled = true;
+            }
+        }
+
+        private void DateOfReceiptTextField_TextChanged(object sender, EventArgs e)
+        {
+            string text = DateOfReceiptTextField.Text.Replace(".", "");
+            bool isNotError = ValidationForm.ValidationIsNumber(text, TextError, Color.Red, "Дата может содержать только цифры !");
+            if (isNotError)
+            {
+                ValidationForm.DateFormatting(DateOfReceiptTextField);
+                isNotError = ValidationForm.DateIsValid(DateOfReceiptTextField.Text, TextError);
+            }
+            ErrorsFormRecords.DateOfReceipt = !isNotError;
+            if (DateOfReceiptTextField.Text == "")
+            {
+                TextError.Text = "";
+                ErrorsFormRecords.DateOfReceipt = false;
+            }
+            if (ErrorsFormRecords.DateOfReceipt && TextError.Text != "")
+                DateOfReceiptTextField.ForeColor = Color.Red;
+            else
+                DateOfReceiptTextField.ForeColor = Color.Black;
+        }
+
+        private void DateOfDischargeTextField_TextChanged(object sender, EventArgs e)
+        {
+            string text = DateOfDischargeTextField.Text.Replace(".", "");
+            bool isNotError = ValidationForm.ValidationIsNumber(text, TextError, Color.Red, "Дата может содержать только цифры !");
+            if (isNotError)
+            {
+                ValidationForm.DateFormatting(DateOfDischargeTextField);
+                isNotError = ValidationForm.DateIsValid(DateOfDischargeTextField.Text, TextError);
+            }
+            ErrorsFormRecords.DateOfDischarge = !isNotError;
+            if (DateOfDischargeTextField.Text == "")
+            {
+                TextError.Text = "";
+                ErrorsFormRecords.DateOfDischarge = false;
+            }
+            if (ErrorsFormRecords.DateOfDischarge && TextError.Text != "")
+                DateOfDischargeTextField.ForeColor = Color.Red;
+            else
+                DateOfDischargeTextField.ForeColor = Color.Black;
+        }
+
+        private void RecordsTable_CellClick(object? sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex != -1) return;
+
+            Func<RecordsViewItem, object> sortingFunc;
+
+            switch (e.ColumnIndex)
+            {
+                case 1:
+                    sortingFunc = record => record.HistoryNumber;
+                    break;
+                case 2:
+                    sortingFunc = record => record.DepartmentID;
+                    break;
+                case 3:
+                    sortingFunc = record => record.DateOfReceipt;
+                    break;
+                case 4:
+                    sortingFunc = record => record.DateOfDischarge;
+                    break;
+                default:
+                    sortingFunc = record => record.MKBCode;
+                    break;
+            }
+
+            if (columnIndex != e.ColumnIndex)
+            {
+                recordDataSource = recordDataSource.OrderBy(sortingFunc).ToList();
+                columnIndex = e.ColumnIndex;
+            }
+            else
+            {
+                List<RecordsViewItem> RecordDataSourceNew = recordDataSource.ToList();
+                RecordDataSourceNew.Reverse();
+                recordDataSource = RecordDataSourceNew;
+            }
+
+            RecordsTable.DataSource = recordDataSource;
+        }
+
+
+    }
+    class RecordsViewItem
+    {
+        public int HistoryNumber { get; set; }
+        public int DepartmentID { get; set; }
+        public DateTime DateOfReceipt { get; set; }
+        public DateTime DateOfDischarge { get; set; }
+        public string MKBCode { get; set; }
+
+    }
+
+    static class ErrorsFormRecords
+    {
+        static public bool HistoryNumber { get; set; } = false;
+        static public bool DepartmentID { get; set; } = false;
+        static public bool DateOfReceipt { get; set; } = false;
+        static public bool DateOfDischarge { get; set; } = false;
+        static public bool MKBCode { get; set; } = false;
     }
 }
